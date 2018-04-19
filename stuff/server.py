55af4e5b6aba1a229b1fdde384a94ebc44da3e32
Simon Christensen <simon_sc@live.dk>
2018-04-19 09:47:26 +0200

    minimal server

--- stuff/server.py
+++ stuff/server.py
@@ -1,1 +1,16 @@
-import socket
+import socketserver
+import threading
+
+class ServerHandler(socketserver.BaseRequestHandler):
+	def handle(self):
+		self.data = self.request.recv(1024).strip()
+		self.request.sendall(self.data)
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), ServerHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
